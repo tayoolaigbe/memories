38c812d015f97b426519e85357baba35c0730e1f
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-06-16 04:13:04 +0100

    Isolated Navbar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
 
-import memories from './images/memories.png';
 import { getPosts } from './actions/posts';
 function App() {
 	const [currentId, setCurrentId] = useState(null);
@@ -19,17 +18,6 @@ function App() {
 
 	return (
 		<Container maxWidth="lg">
-			<AppBar className={classes.appBar} position="static" color="inherit">
-				<Typography className={classes.heading} variant="h2" align="center">
-					Memories
-				</Typography>
-				<img
-					className={classes.image}
-					src={memories}
-					alt="memories"
-					height="60"
-				/>
-			</AppBar>
 			<Grow in>
 				<Container>
 					<Grid
