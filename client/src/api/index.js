f3acc1101c848ba0484859425f9991298146f662
Eyitayo Olaigbe <48358258+tayoolaigbe@users.noreply.github.com>
2021-06-16 03:18:40 +0100

    Add files via upload

---
+++ client/src/api/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const url = 'http://localhost:5000/posts';
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = post => axios.post(url, post);
+export const updatePost = (id, updatedPost) =>
+	axios.patch(`${url}/${id}`, updatedPost);
+export const likePost = id => axios.patch(`${url}/${id}/likePost`);
+export const deletePost = id => axios.delete(`${url}/${id}`);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import posts from './posts';
+
+const reducer = combineReducers({
+	posts,
+});
+
+export default reducer;
