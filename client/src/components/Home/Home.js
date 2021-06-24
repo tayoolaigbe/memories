2e86e6692b4780146b91cd5c04f1f083ac48b0a7
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-06-24 13:37:43 +0100

    Completed Refactor of App Component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination/Pagination';
 
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
@@ -23,6 +24,9 @@ const Home = () => {
 					</Grid>
 					<Grid item xs={12} sm={4}>
 						<Form currentId={currentId} setCurrentId={setCurrentId} />
+						<Paper elevation={6}>
+							<Pagination />
+						</Paper>
 					</Grid>
 				</Grid>
 			</Container>
